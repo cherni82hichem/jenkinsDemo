0e1031fbca8d7565c905c7398a0b8a4c6c2c10a9
hichem <cherni82hichem@yahoo.fr>
2024-02-27 16:51:39 +0100

    for pipeline commit

--- src/main/java/com/jenkins/jenkinsDemo/JenkinsDemoApplication.java
+++ src/main/java/com/jenkins/jenkinsDemo/JenkinsDemoApplication.java
@@ -12,7 +12,7 @@ public class JenkinsDemoApplication {
 	public static Logger logger = LoggerFactory.getLogger(JenkinsDemoApplication.class);
 	@PostConstruct
 	public  void log(){
-		logger.info("PostConstruct:Application started ***************************************");
+		logger.info("Application started ***************************************");
 	}
 
 	public static void main(String[] args) {
