94dbfdadd6d81a20222cfeb528b4c625c030268a
hichem <cherni82hichem@yahoo.fr>
2024-02-27 10:40:06 +0100

    first commit

---
+++ src/test/java/com/jenkins/jenkinsDemo/JenkinsDemoApplicationTests.java
@@ -0,0 +1,18 @@
+package com.jenkins.jenkinsDemo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDemoApplicationTests {
+	public static Logger logger = LoggerFactory.getLogger(JenkinsDemoApplicationTests.class);
+	@Test
+	void contextLoads() {
+		logger.info("Test Application executed **************************************");
+		Assertions.assertEquals(true,true);
+	}
+
+}
